e08ccc1cce02773edaecfa2a61eb9452cf9866d0
Ram Babu <ram.kottapally@gmail.com>
2023-01-04 23:41:05 +0530

    add cummulative cases

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="COVID-19 India | Dashboard", page_icon="💉", layout="wide")
 st.title("COVID-19 Cases in India")
@@ -10,18 +11,23 @@ st.title("COVID-19 Cases in India")
 df = pd.DataFrame(np.random.randn(500, 2) /[10,10] + [20.5937, 78.9629],
 columns=['lat', 'lon'])
 # st.map(df)
-# url = 'https://api.covid19tracker.in/data/static/data.min.json'
+
 url = 'https://data.incovid19.org/v4/min/data.min.json'
 def get_timeseries_data(code: str) -> list:
     """Returns the timeseries data"""
     timeseries_url = f"https://data.incovid19.org/v4/min/timeseries-{code}.min.json"
     resp = requests.get(timeseries_url).json()
-    output_data = {'confirmed': [], 'deceased': [], 'recovered': []}
     all_dates = resp['TT']['dates'] 
+    output_data = {'confirmed': [], 'deceased': [], 'recovered': [], 'active': [], 'labels': list(all_dates.keys())}
+    
     for key in all_dates.keys():
         output_data['confirmed'].append(all_dates.get(key).get('total').get('confirmed'))
         output_data['deceased'].append(all_dates.get(key).get('total').get('deceased'))
         output_data['recovered'].append(all_dates.get(key).get('total').get('recovered'))
+        if output_data['confirmed'][-1] and output_data['deceased'][-1] and output_data['recovered'][-1]:
+            output_data['active'].append(output_data['confirmed'][-1] - (output_data['deceased'][-1] + output_data['recovered'][-1]))
+        else:
+            output_data['active'].append(None)
 
     return output_data
 
@@ -74,3 +80,43 @@ option = st.selectbox('Select State/UT', drop_down_options)
 option_code = option[-4:].strip('()')
 print(option_code)
 timeseries_data = get_timeseries_data('TT')
+
+st.header("Cummulative Cases")
+confirmed_chart, active_chart = st.columns(spec=2, gap="medium")
+with confirmed_chart:
+    st.subheader('Total Confirmed')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('confirmed'), mode='lines+markers', name='Confirmed'))
+    fig.update_traces(line_color='#13bdfa')
+    # fig = px.line(timeseries_data, x=timeseries_data.get('labels'), y=timeseries_data.get('confirmed'), title='Total Confirmed Cases', markers=True)
+    st.plotly_chart(figure_or_data=fig, use_container_width=True,sharing="streamlit")
+
+with active_chart:
+    # Active Chart
+    st.subheader('Total Active')
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('active'), mode='lines+markers', name='Active'))
+    fig1.update_traces(line_color='#f2bb30')
+    st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+
+deceased_chart, recovered_chart = st.columns(spec=2, gap="medium")
+# Deceased Chart
+with deceased_chart:
+    st.subheader('Total Deceased')
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('deceased'), mode='lines+markers', name='Deceased'))
+    fig1.update_traces(line_color='#f70b0b')
+    st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+# st.line_chart(data=timeseries_data.get('confirmed'), x=timeseries_data.get('labels'))
+# st.line_chart(data=timeseries_data.get('deceased'), x=timeseries_data.get('labels'))
+# st.line_chart(data=timeseries_data.get('recovered'), x=timeseries_data.get('labels'))
+# fig.show()
+
+# Recovered Chart
+with recovered_chart:
+    st.subheader('Total Recovered')
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('recovered'), mode='lines+markers', name='Recovered'))
+    fig1.update_traces(line_color='#44f534')
+    st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+
