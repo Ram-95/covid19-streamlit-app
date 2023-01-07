a79fd4c3635e4c903506e0829dee2f8d3e185d5c
Ram Babu <ram.kottapally@gmail.com>
2023-01-07 22:50:10 +0530

    add states code to dashboard

--- app/main.py
+++ app/main.py
@@ -5,14 +5,22 @@ import numpy as np
 from datetime import datetime
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="COVID-19 India | Dashboard", page_icon="💉", layout="wide")
-st.title("COVID-19 Cases in India")
+st.set_page_config(page_title="COVID-19 India | Dashboard", page_icon="📈", layout="wide")
+config = {'displayModeBar': False}  # configuration to hide the plotly toolbar
 
 df = pd.DataFrame(np.random.randn(500, 2) /[10,10] + [20.5937, 78.9629],
 columns=['lat', 'lon'])
 # st.map(df)
+state_code_dict = {
+        'AN': 'Andaman and Nicobar', 'AP': 'Andhra Pradesh', 'AR': 'Arunachal Pradesh', 'AS': 'Assam', 'BR': 'Bihar', 'TT': 'India',
+        'CH': 'Chandigarh', 'CT': 'Chattisgarh', 'DL': 'Delhi', 'DN': 'Dadra and Nagar Haveli', 'GA': 'Goa', 'GJ': 'Gujarat',
+        'HP': 'Himachal Pradesh', 'HR': 'Haryana', 'JH': 'Jharkhand', 'JK': 'Jammu and Kashmir', 'KA': 'Karnataka', 'KL': 'Kerala',
+        'LA': 'Ladakh', 'LD': 'Lakshadweep', 'MH': 'Maharashtra', 'ML': 'Meghalaya', 'MN': 'Manipur', 'MP': 'Madhya Pradesh',
+        'MZ': 'Mizoram', 'NL': 'Nagaland', 'OR': 'Odisha', 'PB': 'Punjab', 'PY': 'Puducherry', 'RJ': 'Rajasthan', 'SK': 'Sikkim',
+        'TG': 'Telangana', 'TN': 'Tamil Nadu', 'TR': 'Tripura', 'UP': 'Uttar Pradesh', 'UT': 'Uttarakhand', 'WB': 'West Bengal'
+    }
 
-url = 'https://data.incovid19.org/v4/min/data.min.json'
+
 def get_timeseries_data(type_of_graph: str, code: str) -> list:
     """Returns the timeseries data"""
     timeseries_url = f"https://data.incovid19.org/v4/min/timeseries-{code}.min.json"
@@ -52,55 +60,50 @@ def plot_data(type_of_graph: str, st_code: str) -> None:
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('confirmed'), mode=graph_mode, name='Confirmed'))
         fig.update_traces(line_color='#13bdfa')
-        st.plotly_chart(figure_or_data=fig, use_container_width=True,sharing="streamlit")
+        st.plotly_chart(figure_or_data=fig, use_container_width=True,sharing="streamlit", config=config)
 
     with active_chart:
         st.subheader('Active')
         fig1 = go.Figure()
         fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('active'), mode=graph_mode, name='Active'))
         fig1.update_traces(line_color='#f2bb30')
-        st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+        st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit", config=config)
 
     # Deceased Chart
     with deceased_chart:
         st.subheader('Deceased')
-        fig1 = go.Figure()
-        fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('deceased'), mode=graph_mode, name='Deceased'))
-        fig1.update_traces(line_color='#f70b0b')
-        st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('deceased'), mode=graph_mode, name='Deceased'))
+        fig2.update_traces(line_color='#f70b0b')
+        st.plotly_chart(figure_or_data=fig2, use_container_width=True,sharing="streamlit", config=config)
 
     # Recovered Chart
     with recovered_chart:
         st.subheader('Recovered')
-        fig1 = go.Figure()
-        fig1.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('recovered'), mode=graph_mode, name='Recovered'))
-        fig1.update_traces(line_color='#44f534')
-        st.plotly_chart(figure_or_data=fig1, use_container_width=True,sharing="streamlit")
+        fig3 = go.Figure()
+        fig3.add_trace(go.Scatter(x=timeseries_data.get('labels'), y=timeseries_data.get('recovered'), mode=graph_mode, name='Recovered'))
+        fig3.update_traces(line_color='#44f534')
+        st.plotly_chart(figure_or_data=fig3, use_container_width=True,sharing="streamlit", config=config)
 
-timeseries_url = 'https://data.incovid19.org/v4/min/timeseries-TN.min.json'
-response = requests.get(url)
-data = response.json()
-state_code_dict = {
-        'AN': 'Andaman and Nicobar', 'AP': 'Andhra Pradesh', 'AR': 'Arunachal Pradesh', 'AS': 'Assam', 'BR': 'Bihar', 'TT': 'India',
-        'CH': 'Chandigarh', 'CT': 'Chattisgarh', 'DL': 'Delhi', 'DN': 'Dadra and Nagar Haveli', 'GA': 'Goa', 'GJ': 'Gujarat',
-        'HP': 'Himachal Pradesh', 'HR': 'Haryana', 'JH': 'Jharkhand', 'JK': 'Jammu and Kashmir', 'KA': 'Karnataka', 'KL': 'Kerala',
-        'LA': 'Ladakh', 'LD': 'Lakshadweep', 'MH': 'Maharashtra', 'ML': 'Meghalaya', 'MN': 'Manipur', 'MP': 'Madhya Pradesh',
-        'MZ': 'Mizoram', 'NL': 'Nagaland', 'OR': 'Odisha', 'PB': 'Punjab', 'PY': 'Puducherry', 'RJ': 'Rajasthan', 'SK': 'Sikkim',
-        'TG': 'Telangana', 'TN': 'Tamil Nadu', 'TR': 'Tripura', 'UP': 'Uttar Pradesh', 'UT': 'Uttarakhand', 'WB': 'West Bengal'
-    }
 
-def load_dashboard():
+def load_dashboard(st_code: str) -> None:
+    """Loads the total summary of COVID-19 cases by the state code."""
+    url = 'https://data.incovid19.org/v4/min/data.min.json'
+    response = requests.get(url)
+    data = response.json()
+    st.title(f"COVID-19 Cases in {state_code_dict.get(st_code)}")
+    last_updated_date = datetime.strptime(data.get(st_code).get('meta').get('last_updated'), '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %H:%M:%S')
+    st.text(f"Data as on: {last_updated_date} (IST)")
     total_confirmed_cases_container, total_active_cases_container, total_deaths_container, total_recovered_cases_container = st.columns(spec=4, gap="large")
-    totals = data.get('TT').get('total')
-    last_updated_date = datetime.strptime(data.get('TT').get('meta').get('last_updated'), '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %H:%M:%S')
+    totals = data.get(st_code).get('total')
     total_active_cases = totals['confirmed'] - (totals['deceased'] + totals['recovered'])
 
-    delta = data.get('TT').get('delta')
+    delta = data.get(st_code).get('delta')
     confirmed_delta = delta.get('confirmed', 0)
     deceased_delta = delta.get('deceased', 0)
     recovered_delta = delta.get('recovered', 0)
     active_delta = confirmed_delta - (deceased_delta + recovered_delta)
-    st.text(f"Data as on: {last_updated_date} (IST)")
+    
 
     with total_confirmed_cases_container:
         st.header(":blue[Confirmed]")
@@ -123,10 +126,10 @@ def load_dashboard():
         st.metric(format(totals.get('recovered'), ","), "", recovered_delta)
 
 
-load_dashboard()
 drop_down_options = [f"{val} ({key})" for key, val in state_code_dict.items()]
 option = st.selectbox('Select State/UT', drop_down_options, index=5)
 option_code = option[-4:].strip('()')
+load_dashboard(option_code)
 
 # Initial Display
 plot_data("Cummulative", option_code)
